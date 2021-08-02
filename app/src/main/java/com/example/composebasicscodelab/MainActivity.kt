3796c65c976bb07f168797d9de8863adcdd57e41
Abdulrahman.Rashad <Abdulrahman.Rashad@warbabank.com>
2021-08-02 16:58:09 +0200

    init commit

--- app/src/main/java/com/example/composebasicscodelab/MainActivity.kt
+++ app/src/main/java/com/example/composebasicscodelab/MainActivity.kt
@@ -10,13 +10,13 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.example.composebasicscodelab.ui.theme.CoposeBasicsCodelabTheme
+import com.example.composebasicscodelab.ui.theme.ComposeBasicsCodeLabTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            CoposeBasicsCodelabTheme {
+            ComposeBasicsCodeLabTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
@@ -37,7 +37,7 @@ fun Greeting(name: String) {
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
-    CoposeBasicsCodelabTheme {
+    ComposeBasicsCodeLabTheme {
         Greeting("Android")
     }
 }
--- app/src/main/java/com/example/composebasicscodelab/ui/theme/Theme.kt
+++ app/src/main/java/com/example/composebasicscodelab/ui/theme/Theme.kt
@@ -28,7 +28,7 @@ private val LightColorPalette = lightColors(
 )
 
 @Composable
-fun CoposeBasicsCodelabTheme(
+fun ComposeBasicsCodeLabTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
     content: @Composable () -> Unit
 ) {
